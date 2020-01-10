5f32a80c011170967a98da6295036165bf3bf166
Anthony Mai <anthony.mai530@gmail.com>
2020-01-10 01:09:58 +0000

    first commit

---
+++ lib/anthony_cli_project.rb
@@ -0,0 +1,6 @@
+require "anthony_cli_project/version"
+
+module AnthonyCliProject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/anthony_cli_project/version.rb
@@ -0,0 +1,3 @@
+module AnthonyCliProject
+  VERSION = "0.1.0"
+end
